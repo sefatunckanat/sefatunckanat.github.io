0f2f8ceda3b7e80ada2212699059c2b780301539
Sefa Tunçkanat <sefatunckanat73@gmail.com>
2016-09-17 17:21:25 +0300

    Mobil sayfada animasyonları filtreleme

--- scripts/main.js
+++ scripts/main.js
@@ -46,7 +46,6 @@ $(document).ready(function () {
     $('.menu li').bind('click', function() {
         //yumuşak kaydırma işlemi
         var hash = $(this).find('span').attr('rel');
-        console.log($(hash).offset().top);
         $('html, body').animate({
             scrollTop: $(hash).offset().top
         },800);
@@ -72,53 +71,55 @@ $(document).ready(function () {
         $(".menu").toggleClass("show-menu");
     }
 
-    // hakkımda kutularının içindeki spanlarin rasgele stilleri
-    var liveSpans = function(){
-        $(".box .texts span").each(function() {
-            var maxW = $(this).closest(".texts").css("width");
-            var maxH = $(this).closest(".texts").css("height");
-            maxW = parseInt(maxW, 10);
-            maxH = parseInt(maxH, 10);
-            var scale = (maxW / 240);
+    if (document.documentElement.clientWidth > 700) {
+        // hakkımda kutularının içindeki spanlarin rasgele stilleri
+        var liveSpans = function(){
+            $(".box .texts span").each(function() {
+                var maxW = $(this).closest(".texts").css("width");
+                var maxH = $(this).closest(".texts").css("height");
+                maxW = parseInt(maxW, 10);
+                maxH = parseInt(maxH, 10);
+                var scale = (maxW / 240);
 
-            var doNot = false;
-            var randomIndex = Math.floor(Math.random()*5);
-            if(randomIndex == $(this).index()){
-                doNot = true;
-            }
-            if(doNot){
-                $(this).fadeIn();
-                var sizes = [20,25,30,18,40];
-                var colors = [
-                    "rgb(120, 5, 207)",
-                    "rgb(214, 187, 2)",
-                    "rgb(94, 201, 36)",
-                    "rgb(201, 36, 36)",
-                    "rgb(238, 224, 33)",
-                    "rgb(11, 25, 120)",
-                    "rgb(184, 7, 213)"
-                ];
-                var size = Math.floor(Math.random() * sizes.length);
-                var posx = Math.floor(Math.random() * maxW);
-                var posy = Math.floor(Math.random() * maxH);
-                var color = Math.floor(Math.random() * colors.length);
-                var rotate = 45-Math.floor(Math.random() * 90);
-                $(this).css("font-size",sizes[size]*(1+Math.random())*scale+"px");
-                $(this).css("left",posx+"px");
-                $(this).css("top",posy+"px");
-                $(this).css("color",colors[color]);
-                $(this).css({
-                  '-webkit-transform' : 'rotate(' + rotate + 'deg)',
-                  '-moz-transform'    : 'rotate(' + rotate + 'deg)',
-                  '-ms-transform'     : 'rotate(' + rotate + 'deg)',
-                  '-o-transform'      : 'rotate(' + rotate + 'deg)',
-                  'transform'         : 'rotate(' + rotate + 'deg)'
-                });
-            }
-        });
+                var doNot = false;
+                var randomIndex = Math.floor(Math.random()*5);
+                if(randomIndex == $(this).index()){
+                    doNot = true;
+                }
+                if(doNot){
+                    $(this).fadeIn();
+                    var sizes = [20,25,30,18,40];
+                    var colors = [
+                        "rgb(120, 5, 207)",
+                        "rgb(214, 187, 2)",
+                        "rgb(94, 201, 36)",
+                        "rgb(201, 36, 36)",
+                        "rgb(238, 224, 33)",
+                        "rgb(11, 25, 120)",
+                        "rgb(184, 7, 213)"
+                    ];
+                    var size = Math.floor(Math.random() * sizes.length);
+                    var posx = Math.floor(Math.random() * maxW);
+                    var posy = Math.floor(Math.random() * maxH);
+                    var color = Math.floor(Math.random() * colors.length);
+                    var rotate = 45-Math.floor(Math.random() * 90);
+                    $(this).css("font-size",sizes[size]*(1+Math.random())*scale+"px");
+                    $(this).css("left",posx+"px");
+                    $(this).css("top",posy+"px");
+                    $(this).css("color",colors[color]);
+                    $(this).css({
+                      '-webkit-transform' : 'rotate(' + rotate + 'deg)',
+                      '-moz-transform'    : 'rotate(' + rotate + 'deg)',
+                      '-ms-transform'     : 'rotate(' + rotate + 'deg)',
+                      '-o-transform'      : 'rotate(' + rotate + 'deg)',
+                      'transform'         : 'rotate(' + rotate + 'deg)'
+                    });
+                }
+            });
+        }
+        var liveSpansLoop = setInterval(liveSpans,5000); // döngü
     }
-    var liveSpansLoop = setInterval(liveSpans,5000); // döngü
-
+    
     $(".box .texts span").each(function() {
         var maxW = parseInt($(this).closest(".texts").css("width"),10);
         var maxH = parseInt($(this).closest(".texts").css("height"),10);
